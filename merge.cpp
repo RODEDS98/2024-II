52daeb5770a4bf91fbb5578d8b46d9c88a53f3ec
Edgard Rodriguez <edgard.rodriguez@ucsp.edu.pe>
2024-09-24 02:56:03 -0500

    Create merge.cpp
    merge Rodriguez

---
+++ merge.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+void merge(int* p, int* q) {      //     p           q             , q - p  = 5
+	int* posfin =q+(q - p-1);     //      2 4 6 8 2   1  3  5  7  9	                  //cout<<*posfin<<endl;
+	while(q<posfin){                // entonces q+5      *  *  *  *
+		for (int* i = p; i < q; i++) {   //           +0 +1 +2 +3 +4
+			if (*i > *q) {
+				int tmp = *i;
+				*i = *q;
+				*q = tmp;
+			}
+		}
+		q++;
+	}
+}
+
+
+//funcion para imprimir
+void imprimir(int* arr, size_t t){
+    for (int* i=arr; i< arr+t; i++) {
+        cout << *i << ", ";
+    }
+    cout <<endl;
+    cout <<endl;
+}
+
+// Funcion para contar los pares  (solo para verificacion)
+int count_par_impar(int* A, size_t t) {  // contar cuantos pares tiene el arreglo
+	int pares = 0;
+	for (int* p = A; p < (A + t); p++) {
+		*p % 2 == 0 ? pares++ : pares = pares;
+	}
+	return pares;
+}
+
+
+int main() {
+    
+    int Arr_1[] = { 0,2,4,6,8,10,12,14,1,3,5,7,9,11,13,15 };
+    //int Arr_1[] = { 2,4,6,8,1,3,5,7 };
+    
+    //generalizacion del tamaño
+    size_t tam = sizeof(Arr_1);
+    tam= tam/4;
+    int mitad=tam/2;
+    cout<<"verificando tamaño: "<<tam<<endl;
+    cout<<"verificando pares: "<<count_par_impar(Arr_1,tam)<<endl<<endl;
+    
+    cout <<"array original"<<endl; //Array original
+    imprimir(Arr_1,tam);
+    
+    cout <<"array ordenado (Merge)"<<endl; //Array ordenado
+    merge(Arr_1,Arr_1 + mitad);
+    imprimir(Arr_1,tam);
+}
