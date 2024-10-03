b87d94913c089a64379cb83a60cc3b347f1a386d
Edgard Rodriguez <edgard.rodriguez@ucsp.edu.pe>
2024-10-03 12:23:12 -0500

    Update cambio.cpp

--- cambio.cpp
+++ cambio.cpp
@@ -1,30 +1,29 @@
-
-#include <iostream>  //556.60
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    // Trabajar con enteros que representen centavos (en lugar de soles)
+    int A[11] = {20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10}; // Monedas en centavos
 
-	float A[11]={200,100,50,20,10,5,2,1,0.5,0.2,0.1};
+    float dinero;
+    int copia;
+    int cociente;
 
-	float dinero;
-	float copia;
-	int cociente;
+    cout << "Ingrese una cantidad en soles\n";
+    cin >> dinero;
 
-	cout << "Ingrese una cantidad en soles\n";
-	cin >> dinero;
-	copia = dinero;
-	//cout<<"copia "<<copia<<endl; 
-	
-	for(float* p = A ; p < A + 11 ; p++ ){
-    
-		cociente = copia/(*p);               // 200/200  = 1 
-		//cout<<"cociente "<<cociente<<endl;
-		
-		if(copia>0) {                        // 200 
-		    copia -= cociente*(*p);          // 200 - 200*1  = 0
-		    //cout<<"copia "<<copia<<endl; 
-		}
-		cout<<"unidades de - "<< *p<<" - :"<< cociente<<endl;
-	}
+    // Convertir el dinero a centavos (usando enteros)
+    copia = static_cast<int>(dinero * 100 + 0.5); // Redondear para evitar errores
+    cout << "Cantidad en centavos: " << copia << endl;
 
+    for (int* p = A; p < A + 11; p++) {
+        cociente = copia / (*p);  // Dividir para encontrar cuántas monedas de *p se pueden dar
+
+        if (copia > 0) {
+            cout << "Unidades de - " << *p / 100.0 << " soles: " << cociente << endl;  // Mostrar monedas en soles
+            copia -= cociente * (*p);  // Restar el valor de las monedas usadas
+        }
+    }
+
+    return 0;
 }
